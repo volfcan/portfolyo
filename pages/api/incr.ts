89f88b96dc56f74994c8c591d07d514048f359d9
volfcan <20183360068@ogr.akdeniz.edu.tr>
2024-01-15 11:25:08 +0300

    fixes

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -1,55 +1,55 @@
-import { Redis } from "@upstash/redis";
-import { NextRequest, NextResponse } from "next/server";
-import { Client } from '@notionhq/client'
+// import { Redis } from "@upstash/redis";
+// import { NextRequest, NextResponse } from "next/server";
+// import { Client } from '@notionhq/client'
 
-const redis = Redis.fromEnv();
+// const redis = Redis.fromEnv();
 
-const notionSecret = process.env.NOTION_SECRET
-const notionDatabaseID = process.env.NOTION_DATABASE_ID
+// const notionSecret = process.env.NOTION_SECRET
+// const notionDatabaseID = process.env.NOTION_DATABASE_ID
 
-const notion = new Client({ auth: notionSecret })
+// const notion = new Client({ auth: notionSecret })
 
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
-export default async function incr(req: NextRequest): Promise<NextResponse> {
-  if (req.method !== "POST") {
-    return new NextResponse("use POST", { status: 405 });
-  }
-  if (req.headers.get("Content-Type") !== "application/json") {
-    return new NextResponse("must be json", { status: 400 });
-  }
+// export default async function incr(req: NextRequest): Promise<NextResponse> {
+//   if (req.method !== "POST") {
+//     return new NextResponse("use POST", { status: 405 });
+//   }
+//   if (req.headers.get("Content-Type") !== "application/json") {
+//     return new NextResponse("must be json", { status: 400 });
+//   }
 
-  const body = await req.json();
-  let slug: string | undefined = undefined;
-  if ("slug" in body) {
-    slug = body.slug;
-  }
-  if (!slug) {
-    return new NextResponse("Slug not found", { status: 400 });
-  }
-  const ip = req.ip;
-  if (ip) {
-    // Hash the IP in order to not store it directly in your db.
-    const buf = await crypto.subtle.digest(
-      "SHA-256",
-      new TextEncoder().encode(ip),
-    );
-    const hash = Array.from(new Uint8Array(buf))
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("");
+//   const body = await req.json();
+//   let slug: string | undefined = undefined;
+//   if ("slug" in body) {
+//     slug = body.slug;
+//   }
+//   if (!slug) {
+//     return new NextResponse("Slug not found", { status: 400 });
+//   }
+//   const ip = req.ip;
+//   if (ip) {
+//     // Hash the IP in order to not store it directly in your db.
+//     const buf = await crypto.subtle.digest(
+//       "SHA-256",
+//       new TextEncoder().encode(ip),
+//     );
+//     const hash = Array.from(new Uint8Array(buf))
+//       .map((b) => b.toString(16).padStart(2, "0"))
+//       .join("");
 
-    // deduplicate the ip for each slug
-    const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
-      nx: true,
-      ex: 24 * 60 * 60,
-    });
-    if (!isNew) {
-      new NextResponse(null, { status: 202 });
-    }
-  }
-  await redis.incr(["pageviews", "projects", slug].join(":"));
-  return new NextResponse(null, { status: 202 });
-}
+//     // deduplicate the ip for each slug
+//     const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
+//       nx: true,
+//       ex: 24 * 60 * 60,
+//     });
+//     if (!isNew) {
+//       new NextResponse(null, { status: 202 });
+//     }
+//   }
+//   await redis.incr(["pageviews", "projects", slug].join(":"));
+//   return new NextResponse(null, { status: 202 });
+// }
